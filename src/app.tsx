54f31be14f09015db8d3bb0bea75ed87a2343e1d
filfai <filfai1003@gmail.com>
2024-09-25 22:41:53 +0200

    init setup css

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import './style.css';
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import "./style.css";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Parallax pages={3}>
-        <ParallaxLayer
-          offset={0}
-          factor={3}
-          speed={0}
-          style={{
-            backgroundImage: 'url(/assets/back1.png)',
-            backgroundSize: 'cover',
-          }}
-        />
-        <ParallaxLayer
-          factor={3}
-          speed={1}
-          offset={1}
-          style={{
-            backgroundImage: 'url(/assets/back2.png)',
-            backgroundSize: 'cover',
-          }}
-        />
-          
-      </Parallax>
+    <div className="column">
+      <div className="multi-row">
+        <div className="box">1dsa</div>
+        <div className="box">2dsa</div>
+        <div className="box">3dsa</div>
+        <div className="box">4dsa</div>
+        <div className="box">5dsa</div>
+        <div className="box">6dsa</div>
+      </div>
+
+      <div className="row">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
     </div>
   );
 };
