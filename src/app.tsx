37d3c04aeb25d1b057c4511e59c6a105d106a8de
filfai <filfai1003@gmail.com>
2024-09-27 22:04:10 +0200

    x

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,156 @@
 import React from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import "./style.css";
 
 const App: React.FC = () => {
   return (
-    <div className="column">
-      <div className="multi-row">
-        <div className="box">1dsa</div>
-        <div className="box">2dsa</div>
-        <div className="box">3dsa</div>
-        <div className="box">4dsa</div>
-        <div className="box">5dsa</div>
-        <div className="box">6dsa</div>
-      </div>
+    <html>
+      <h1>Filfai</h1>
+      <h1>Main projects</h1>
+      <div
+        className="multi-row"
+        style={{
+          justifyContent: "center",
+          justifyItems: "center",
+          alignContent: "center",
+          alignItems: "center",
+          gap: 80,
+        }}
+      ></div>
 
-      <div className="row">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+      <h1>Curriculum</h1>
+      <div
+        className="multi-row"
+        style={{
+          justifyContent: "center",
+          justifyItems: "center",
+          alignContent: "center",
+          alignItems: "center",
+          gap: 80,
+        }}
+      >
+        <div className="column" style={{ gap: 10 }}>
+          <img src="/assets/curriculum.png" alt="" height="400" />
+          <button>Download</button>
+        </div>
+        <div
+          className="multi-row"
+          style={{
+            justifyContent: "space-evenly",
+            justifyItems: "space-evenly",
+            alignContent: "center",
+            alignItems: "center",
+            gap: 40,
+          }}
+        >
+          <div className="column" style={{ gap: 10 }}>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/it.png" alt="" width="50" height="50" />
+              <p>Italian</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/fr.png" alt="" width="50" height="50" />
+              <p>French</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/en.png" alt="" width="50" height="50" />
+              <p>English</p>
+            </div>
+          </div>
+          <div className="column" style={{ gap: 10 }}>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/java.png" alt="" width="50" height="50" />
+              <p>Java</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/c.png" alt="" width="50" height="50" />
+              <p>C</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/sql.png" alt="" width="50" height="50" />
+              <p>SQL</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/js.png" alt="" width="50" height="50" />
+              <p>JavaScript</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/html.png" alt="" width="50" height="50" />
+              <p>HTML 5</p>
+            </div>
+            <div
+              className="row"
+              style={{
+                alignContent: "start",
+                alignItems: "start",
+                gap: 10,
+              }}
+            >
+              <img src="/assets/css.png" alt="" width="50" height="50" />
+              <p>CSS</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <footer>
+        <h2>Contact me</h2>
+      </footer>
+    </html>
   );
 };
 
